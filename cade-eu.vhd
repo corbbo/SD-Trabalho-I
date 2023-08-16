09049979edac0c81d5d7289d15e3c93c37815139
Thiago Zilberknop <134622097+corbbo@users.noreply.github.com>
2023-08-16 09:10:58 -0300

    Update cade-eu.vhd

--- cade-eu.vhd
+++ cade-eu.vhd
@@ -12,18 +12,29 @@ entity cade_eu is
     point     :    in  std_logic;
     address   :    out std_logic_vector(11 downto 0);
     finish    :    out std_logic;
-    room      :    out std_logic_vector(3 downto 0);
+    room      :    out std_logic_vector(3 downto 0)
   );
 end cade_eu;
 
 architecture arq of cade_eu is
   type coord is record
-  x:  std_logic_vector(5 downto 0);
-  y:  std_logic_vector(5 downto 0);
+    x:  std_logic_vector(5 downto 0);
+    y:  std_logic_vector(5 downto 0);
   end record;
+  type state is (init, idle, search_down, search_up, search_left, search_right, src_X0, src_Y0, src_X1, src_Y1, retorno);
+  signal EA, PE: state;
+  signal is_room, reset: std_logic := '0';
+  signal has_wall: std_logic_vector(3 downto 0) := "0000";
   constant N_ROOM: integer := 8;
   type ROOM is array(0 to N_ROOM) of coord;
   signal salas : ROOM;
 begin
-  
+  process FSD (reset, clock)
+  begin
+    if reset = '1' then
+      --...
+    else if clock'event and clock = '1' then
+      EA <= PE;
+    end if;
+  end process FSD;
     
