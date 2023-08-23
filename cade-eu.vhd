8d4518cd59fd5aff953d1923c7ca81d004437ea4
Thiago Zilberknop <134622097+corbbo@users.noreply.github.com>
2023-08-23 09:17:07 -0300

    Update cade-eu.vhd

--- cade-eu.vhd
+++ cade-eu.vhd
@@ -23,7 +23,7 @@ architecture arq of cade_eu is
   end record;
   constant N_ROOM: integer := 8;
   signal ponto_de_teste: coord;
-  type state is (init, idle, search_down, search_up, search_left, search_right, src_X0, src_Y0, src_X1, src_Y1, retorno);
+  type state is (init, idle, search_down, set_wall_down, search_up, set_wall_up, search_left, set_wall_left, search_right, set_wall_right, src_X0, set_wall_srcX0, src_Y0, set_wall_srcY0, src_X1, set_wall_srcX1, src_Y1, set_wall_srcY1, retorno);
   signal EA, PE: state;
   signal is_room: std_logic;
   signal has_wall: std_logic_vector(3 downto 0);
@@ -34,85 +34,75 @@ begin
   process(reset, clock)
   begin
     if reset = '1' then
-      EA <= idle;
-      is_room <= '0';
-      has_wall <= "0000"; --UP , DOWN , LEFT , RIGHT
-      room <= "0000";
-      address <= x"00";
-      finish <= '0';
+        EA <= idle;
+        is_room <= '0';
+        has_wall <= "0000"; --UP , DOWN , LEFT , RIGHT
+        room <= "0000";
+        address <= x"00";
+        finish <= '0';
     else if clock'event and clock = '1' then
-      case EA is
-              when init =>
-                      ponto_de_teste.x <= x; -- pega o ponto que vai testar e coloca ele em coord.x
-                      ponto_de_teste.y <= y; -- pega o ponto que vai testar e coloca ele em coord.y
+        case EA is
+            when init => ponto_de_teste.x <= x; -- pega o ponto que vai testar e coloca ele em coord.x
+                         ponto_de_teste.y <= y; -- pega o ponto que vai testar e coloca ele em coord.y
 
-              when search_up => --while !(is wall) volta pra search_up
-                      address <= ponto_de_teste.y & ponto_de_teste.x;
-                        has_wall(3) => '1'; -- 3 downto 0, bit 3 setado pra 1
-
-              when search_down => 
-                        has_wall(2) => '1'; -- 3 downto 0, bit 2 setado pra 1
-              when search_left => 
-                        has_wall(1) => '1'; -- 3 downto 0, bit 1 setado pra 1
-              when search_right => 
-                        has_wall(0) => '1'; -- 3 downto 0, bit 0 setado pra 1
-              when src_X0 =>
-              when src_Y0 =>
-              when src_X1 =>
-              when src_Y1 =>
-              when retorno =>
-
-      EA <= PE;
+            when search_up, search_down, search_left, search_right => address <= ponto_de_teste.y & ponto_de_teste.x;
+              
+            when src_X0, src_X1, src_Y0, src_Y1 => address <= ponto_de_teste.y & ponto_de_teste.x;
+              
+            when retorno =>
+        end case;
+    EA <= PE;
     end if;
-  end process;
+  end process
+
   -- logica de estados
   process(EA, find)
   begin
     case EA is
-      when idle =>  
-      if find = '1' then
-                      PE <= init;
-                    else PE <= idle;
+      when idle =>      if find = '1' then
+                            PE <= init;
+                            else PE <= idle;
+                        end if;
 
-      when init =>
-                      PE <= search_up;
+      when init =>      PE <= search_up;
+
       when search_up => --while !(is wall) volta pra search_up
-                      if point = '0' and ponto_de_teste.y /= '0' then --se não achou parede, nem terminou a grade, continua procurando
-                        PE <= search_up;
-                      else if point = '1' then
-                        -- tem que setar "has_wall" => '1000' e PE => search_down
-                        PE <= search_down;
-                      else others
-                        PE <= idle;
+                            if point = '0' and ponto_de_teste.y /= "00000" then -- se não achou parede, nem terminou a grade, continua procurando
+                                PE <= search_up;
+                                else if point = '1' then -- achou parede
+                                    PE <= search_down;
+                                else others -- a grade terminou sem achar parede, logo não é uma sala
+                                    PE <= retorno;
+                            end if;
 
+      when search_down =>   if point = '0' and ponto_de_teste.y /= "11111" then --se não achou parede, nem terminou a grade, continua procurando
+                                PE <= search_down;
+                                else if point = '1' then -- achou parede
+                                    PE <= search_left;
+                                else others
+                                    PE <= retorno;
+                            end if;
 
-      when search_down => 
-                      if point = '0' and ponto_de_teste.y /= '63' then --se não achou parede, nem terminou a grade, continua procurando
-                        PE <= search_down;
-                      else if point = '1' then
-                        -- tem que setar "has_wall" => '1000' e PE => search_down
-                        PE <= search_left;
-                      else others
-                        PE <= idle;
+      when search_left =>   if point = '0' and ponto_de_teste.x /= "00000" then --se não achou parede, nem terminou a grade, continua procurando
+                                PE <= search_left;
+                                else if point = '1' then -- achou parede
+                                    PE <= search_right;
+                                else others
+                                    PE <= retorno;
+                            end if;
 
-      when search_left => 
-                      if point = '0' and ponto_de_teste.x /= '0' then --se não achou parede, nem terminou a grade, continua procurando
-                        PE <= search_left;
-                      else if point = '1' then
-                        -- tem que setar "has_wall" => '1000' e PE => search_down
-                        PE <= search_right;
-                      else others
-                        PE <= idle;
-      when search_right => 
-                      if point = '0' and ponto_de_teste.x /= '63' then --se não achou parede, nem terminou a grade, continua procurando
-                        PE <= search_right;
-                      else if point = '1' then
-                        -- tem que setar "has_wall" => '1000' e PE => search_down
-                        PE <= src_X0;
-                      else others
-                        PE <= idle;
-      when src_X0 =>
-                      PE <= src_Y0;
+      when search_right =>  if point = '0' and ponto_de_teste.x /= "11111" then --se não achou parede, nem terminou a grade, continua procurando
+                                PE <= search_right;
+                                else if point = '1' then
+                                    if has_wall = "1111" then -- continua procurando apenas se tiver encontrado paredes em todos os lados (failsafe)
+                                        PE <= src_X0;
+                                    end if;
+                                else others
+                                    PE <= retorno;
+                            end if;
+
+      when src_X0 =>        
+                            PE <= src_Y0;
       when src_Y0 =>
                       PE <= src_X1;
       when src_X1 =>
