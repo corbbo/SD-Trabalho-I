54a96a0b638aa70fce47a8678ff052b91c3c14fb
LuigiSalvatore <134612508+LuigiSalvatore@users.noreply.github.com>
2023-08-23 08:37:59 -0300

    add logica dos estados

--- cade-eu.vhd
+++ cade-eu.vhd
@@ -66,7 +66,7 @@ begin
   begin
     case EA is
       when idle =>  
-      if prog = '1' then
+      if find = '1' then
                       PE <= init;
                     else PE <= idle;
 
@@ -79,17 +79,38 @@ begin
                         -- tem que setar "has_wall" => '1000' e PE => search_down
                         has_wall(3) => '1'; -- 3 downto 0, bit 3 setado pra 1
                         PE <= search_down;
-                      else if ponto_de_teste.y = '0' then --se y 'estoura', não é uma sala.
-                        PE <= init;
                       else others
-                        PE <= init;
+                        PE <= idle;
+
 
       when search_down => 
-                      PE <= search_left;
+                            if point = '0' and ponto_de_teste.y /= '63' then --se não achou parede, nem terminou a grade, continua procurando
+                        PE <= search_down;
+                      else if point = '1' then
+                        -- tem que setar "has_wall" => '1000' e PE => search_down
+                        has_wall(2) => '1'; -- 3 downto 0, bit 2 setado pra 1
+                        PE <= search_left;
+                      else others
+                        PE <= idle;
+
       when search_left => 
-                      PE <= search_right;
+                      if point = '0' and ponto_de_teste.x /= '0' then --se não achou parede, nem terminou a grade, continua procurando
+                        PE <= search_left;
+                      else if point = '1' then
+                        -- tem que setar "has_wall" => '1000' e PE => search_down
+                        has_wall(1) => '1'; -- 3 downto 0, bit 1 setado pra 1
+                        PE <= search_right;
+                      else others
+                        PE <= idle;
       when search_right => 
-                      PE <= src_X0;
+                      if point = '0' and ponto_de_teste.x /= '63' then --se não achou parede, nem terminou a grade, continua procurando
+                        PE <= search_right;
+                      else if point = '1' then
+                        -- tem que setar "has_wall" => '1000' e PE => search_down
+                        has_wall(0) => '1'; -- 3 downto 0, bit 1 setado pra 1
+                        PE <= src_X0;
+                      else others
+                        PE <= idle;
       when src_X0 =>
                       PE <= src_Y0;
       when src_Y0 =>
